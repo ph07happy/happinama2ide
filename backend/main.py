c8b84c4d019e7ea41628d7f58b78146e4a9accce
Happy Kumar <150436396+ph07happy@users.noreply.github.com>
2026-06-26 21:45:29 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://happinama2ide-7jpz.onrender.com",
+        "https://happinama2ide.vercel.app",
         "http://localhost:5500",
         "http://127.0.0.1:5500"
     ],
